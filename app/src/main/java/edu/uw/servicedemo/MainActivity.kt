0f349231ec2c2d53d2750d6a4b63f96b1e5338ba
Joel Ross <joelross@uw.edu>
2018-11-19 11:44:07 -0800

    Reset for start of lecture

--- app/src/main/java/edu/uw/servicedemo/MainActivity.kt
+++ app/src/main/java/edu/uw/servicedemo/MainActivity.kt
@@ -1,46 +1,37 @@
 package edu.uw.servicedemo
 
-import android.content.ComponentName
-import android.content.Context
-import android.content.Intent
-import android.content.ServiceConnection
+import android.media.MediaPlayer
 import android.os.AsyncTask
 import android.os.Bundle
-import android.os.IBinder
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.TextView
 import android.widget.Toast
 
-class MainActivity : AppCompatActivity(), ServiceConnection {
+class MainActivity : AppCompatActivity() {
 
     private val TAG = "Main"
-
-    private lateinit var mService: MusicService
-    private var mServiceBound: Boolean = false
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    //when "Start" button is pressed
-    fun handleStart(v: View) {
+    //when "Start Count" button is pressed
+    fun handleStartCount(v: View) {
         Log.i(TAG, "Start pressed")
 
         //on main thread
-//        for(count in 0..10){
-//            Log.v(TAG, "Count: $count");
-//            try {
-//                Thread.sleep(2000); //sleep for 2 seconds
-//            } catch (e: InterruptedException) {
-//                Thread.currentThread().interrupt();
-//            }
-//        }
+        for(count in 0..10){
+            Log.v(TAG, "Count: $count");
+            try {
+                Thread.sleep(2000); //sleep for 2 seconds
+            } catch (e: InterruptedException) {
+                Thread.currentThread().interrupt();
+            }
+        }
 
 
         //using java thread
@@ -52,14 +43,12 @@ class MainActivity : AppCompatActivity(), ServiceConnection {
 //        task.execute(1, 5);
 
         //using service
-        startService(Intent(this@MainActivity, CountingService::class.java))
+        //TODO
     }
 
-    //when "Stop" button is pressed
-    fun handleStop(v: View) {
+    //when "StopCount" button is pressed
+    fun handleStopCount(v: View) {
         Log.i(TAG, "Stop pressed")
-
-        stopService(Intent(this@MainActivity, CountingService::class.java))
     }
 
     //Thread runner version
@@ -112,50 +101,38 @@ class MainActivity : AppCompatActivity(), ServiceConnection {
 
 
 
-    /* Media controls */
+    /******************
+     * Media controls *
+     ******************/
+
+    private var mediaPlayer: MediaPlayer? = null
+
     fun playMedia(v: View) {
-        startService(Intent(this, MusicService::class.java))
+        if (mediaPlayer == null) {
+            mediaPlayer = MediaPlayer.create(this, R.raw.scott_joplin_the_entertainer_1902)
+        }
+        mediaPlayer!!.start()
     }
 
     fun pauseMedia(v: View) {
-        if (mServiceBound) {
-            mService.pauseMusic()
-        }
+        mediaPlayer!!.pause()
     }
 
     fun stopMedia(v: View) {
-        if (mServiceBound) {
-            mService.stopMusic()
+        if(mediaPlayer != null) {
+            mediaPlayer!!.stop()
+            mediaPlayer!!.release()
+            mediaPlayer = null
         }
-        //stopService(new Intent(MainActivity.this, MusicService.class));
     }
 
 
     override fun onStart() {
-        val intent = Intent(this@MainActivity, MusicService::class.java)
-        bindService(intent, this, Context.BIND_AUTO_CREATE)
         super.onStart()
     }
 
     override fun onStop() {
-        if (mServiceBound) {
-            unbindService(this)
-        }
         super.onStop()
-    }
-
-    override fun onServiceConnected(name: ComponentName, service: IBinder) {
-        Log.v(TAG, "Service bound")
-        val binder = service as MusicService.LocalBinder //cast to specific type
-        (findViewById<View>(R.id.txtSongTitle) as TextView).text = binder.getSongName()
-
-        mService = binder.getService() //save reference to the service
-        mServiceBound = true
-    }
-
-    override fun onServiceDisconnected(name: ComponentName) {
-        Log.v(TAG, "Service unbound")
-        mServiceBound = false
     }
 
     override fun onDestroy() {
